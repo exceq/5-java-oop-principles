79999e539f76c9f0bfda7d36acb508c18f2c1b03
M <mihail.petuxov@yandex.ru>
2021-03-11 09:21:43 +0500

    merge all

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,11 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger l1 =  new Logger("name");
+        l1.setLevel(Level.INFO);
+        l1.info("info");
+        l1.debug("debug");
+        l1.warning("warning");
+        l1.error("%s + %s = [[[","qw", "er");
     }
 }
--- task02/src/com/example/task02/Bill.java
+++ task02/src/com/example/task02/Bill.java
@@ -44,7 +44,7 @@ public class Bill {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("Счет к оплате\n");
+        StringBuilder sb = new StringBuilder("Bill To Pay\n");
         for(BillItem item : items) {
             sb.append(item.item.getName()).append('(').append(item.amount).append(')');
             sb.append('\n');
